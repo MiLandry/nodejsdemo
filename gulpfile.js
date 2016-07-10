39905aa0de3c2ce778c853c535052a2802a24a97
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-07-09 21:15:18 -0400

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 var  jsFiles = ['*.js', 'src/**/*.js']; 
 
 gulp.task('style', function() //create a gulp task called "style", which will execute when you call gulp style
 {
-    gulp.src(jsFiles)  //bring in all js files from this dir and src (ie not from node_modules) that we want to lint
+    return gulp.src(jsFiles)  //bring in all js files from this dir and src (ie not from node_modules) that we want to lint
     .pipe(jshint()) //pipe it into js hint, which will execute
     .pipe(jshint.reporter('jshint-stylish', { //which we must then pipe into a reporter)
         verbose:true
-    }));
+    }))
+    .pipe(jscs());
 });
